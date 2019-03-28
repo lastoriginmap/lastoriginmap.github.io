ba1b19f3d40f8a3d5fdf7d9eaca0532119ec56c9
sonagi784 <sonagi784@gmail.com>
2019-03-28 18:02:48 +0900

    test

--- js/area.js
+++ js/area.js
@@ -1,5 +1,5 @@
-function show_enemy(enemy_name) {
+function show_enemy(url) {
     var popupX = (window.screen.width / 2) - (450 / 2);
     var popupY= (window.screen.height /2) - (400 / 2);
-    window.open(enemy_name, "popup_enemy", 'status=no, height=400, width=463, left='+ popupX + ', top='+ popupY + ', screenX='+ popupX + ', screenY= '+ popupY);
+    window.open(url, "popup_enemy", 'status=no, height=400, width=463, left='+ popupX + ', top='+ popupY + ', screenX='+ popupX + ', screenY= '+ popupY);
 }
