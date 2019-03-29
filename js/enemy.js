eea7af31d5ae7b5b802a97035d3eb1c58075b487
sonagi784 <sonagi784@gmail.com>
2019-03-29 18:42:57 +0900

    5-8ex

---
+++ js/enemy.js
@@ -0,0 +1,18 @@
+temp = location.href.split("?");
+data = temp[1].split("&");
+skill = [];
+for (i in data) {
+    if (data[i] == -1) {
+        data[i] = '???';
+    }
+}
+HP = data.shift();
+strength = data.shift();
+critical = data.shift() + '%';
+defense = data.shift();
+accuracy = data.shift() + '%';
+agility = data.shift();
+dodge = data.shift() + '%';
+for (damage of data) {
+    skill.push(damage);
+}
