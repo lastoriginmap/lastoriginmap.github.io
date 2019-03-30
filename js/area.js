6afdef7f7af7bae284d3abdf557c840844c0d204
sonagi784 <sonagi784@gmail.com>
2019-03-30 16:10:41 +0900

    철충 정보 업데이트

--- js/area.js
+++ js/area.js
@@ -3,3 +3,11 @@ function show_enemy(url) {
     var popupY= (window.screen.height /2) - (400 / 2);
     window.open(url, "popup_enemy", 'status=no, height=400, width=463, left='+ popupX + ', top='+ popupY + ', screenX='+ popupX + ', screenY= '+ popupY);
 }
+function get_url() {
+    url = ''
+    url += (arguments[0] + '?');
+    for(var i = 1; i < arguments.length; i++) {
+        url += (arguments[i] + '&');
+    }
+    return url;
+}
