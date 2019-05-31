036f4b9ba16b91a1649b08e6b8c6c97cac1b0065
ImpMK <imp_mk@naver.com>
2019-05-31 14:46:53 +0900

    Add responsive UI

--- js/enemy.js
+++ js/enemy.js
@@ -10,43 +10,45 @@ window.onload = function(){
 
 function drawEnemyPage(enemyStatData, enemyDescData)
 {
-	document.title = enemyStatData.name+' 정보'		;
-	$('#enemy_name').append(enemyStatData.name);
-	writeData('enemy_LVL', enemyStatData.LVL);
-	writeData('enemy_HP', enemyStatData.HP);
-	writeData('enemy_ATK', enemyStatData.ATK);
-	writeData('enemy_DEF', enemyStatData.DEF);
-	writeData('enemy_AGI', enemyStatData.AGI);
-	writeData('enemy_CRT', enemyStatData.CRT);
-	writeData('enemy_HIT', enemyStatData.HIT);
-	writeData('enemy_DOD', enemyStatData.DOD);
+	document.title = enemyStatData.name+' 정보';
+	$('#name').append(enemyStatData.name);
+	$('.image').append("<img src=\"https://raw.githubusercontent.com/ImpMK/lastorigin_helper/canvas/images/"+enemyDescData.img+"\" style=\"width: 100%\" />");
+	writeData('LVL', enemyStatData.LVL);
+	writeData('HP', enemyStatData.HP);
+	writeData('ATK', enemyStatData.ATK);
+	writeData('DEF', enemyStatData.DEF);
+	writeData('AGI', enemyStatData.AGI);
+	writeData('CRT', enemyStatData.CRT);
+	writeData('HIT', enemyStatData.HIT);
+	writeData('DOD', enemyStatData.DOD);
 	
 	for(var i=0;i<enemyDescData.skills.length;i++)
 	{
 		$('.carousel-indicators').append('<li></li>');
 		$('li:last').attr({
-			"data-target": "#skill_carousel",
+			"data-target": "#skill-carousel",
 			"data-slide-to": String(i)
 		});
-		if(enemyDescData.skills[i].type=="active") { $('li:last').attr("class","skill_active") }
-		else if(enemyDescData.skills[i].type=="passive") { $('li:last').attr("class","skill_passive") }
+		if(enemyDescData.skills[i].type=="active") { $('li:last').attr("class","skill-active") }
+		else if(enemyDescData.skills[i].type=="passive") { $('li:last').attr("class","skill-passive") }
 		
 		$('.carousel-inner').append('<div id="inner'+String(i)+'"></div>\n');
 		$('#inner'+String(i)).attr({
 			"class": "item",
 		});
-		$('#inner'+String(i)).append('<div class=\"skill_wrap\"><div class=\"row\"><div class=\"col-xs-4\"><div class=\"skill_name_wrap\"></div></div><div class=\"col-xs-3\"><div class=\"skill_range_wrap\"></div></div><div class=\"col-xs-5\"><div class=\"skill_area_wrap\"><table><tr class=\"row\"><td></td><td></td><td></td></tr><tr class=\"row\"><td></td><td></td><td></td></tr><tr class=\"row\"><td></td><td></td><td></td></tr></table></div></div></div><div class=\"row\"><div class=\"col-xs-12\"><div class=\"skill_description_wrap\"></div></div></div></div>');
-		$('div.col-xs-4:last > div.skill_name_wrap').append(enemyDescData.skills[i].name);
-		$('div.col-xs-3:last > div.skill_range_wrap').append(enemyDescData.skills[i].range);
-		//$('div.col-xs-5:last > div.skill_area_wrap').append('<table><tr class=\"row\"><td></td><td></td><td></td></tr><tr class=\"row\"><td></td><td></td><td></td></tr><tr class=\"row\"><td></td><td></td><td></td></tr></table>');
-		if(typeof enemyDescData.skills[i].areadata!="undefined") { drawSkillArea($('div.col-xs-5:last > div.skill_area_wrap'), enemyDescData.skills[i].areadata); }
-		$('div.col-xs-12:last > div.skill_description_wrap').append('<p>'+enemyDescData.skills[i].description+'</p>');
+		$('#inner'+String(i)).append('<div class=\"row\"><div class=\"skill-container\"><div class=\"col-xs-8 col-sm-8 skill-name-wrap\"></div><div class=\"col-xs-4 col-sm-2 col-sm-push-2 skill-area-wrap\"><table><tr class=\"row\"><td></td><td></td><td></td></tr><tr class=\"row\"><td></td><td></td><td></td></tr><tr class=\"row\"><td></td><td></td><td></td></tr></table></div><div class=\"col-xs-8 col-sm-2 col-sm-pull-2 skill-range-wrap\"></div><div class=\"col-xs-12 skill-description-wrap\"></div></div></div>');
+		$('.skill-name-wrap:last').append("<h4> Lv. "+enemyStatData.skillLVL[i]+" "+enemyDescData.skills[i].name+"</h4>");
+		$('.skill-range-wrap:last').append("사정거리 "+enemyDescData.skills[i].range);
+		//$('div.col-xs-5:last > div.skill-area-wrap').append('<table><tr class=\"row\"><td></td><td></td><td></td></tr><tr class=\"row\"><td></td><td></td><td></td></tr><tr class=\"row\"><td></td><td></td><td></td></tr></table>');
+		if(typeof enemyDescData.skills[i].areadata!="undefined") { drawSkillArea($('.skill-area-wrap:last'), enemyDescData.skills[i].areadata); }
+		$('.skill-description-wrap:last').append('<p>'+enemyDescData.skills[i].description+'</p>');
 		$('#'+enemyDescData.skills[i].title.substr(0,6)+'power'+enemyDescData.skills[i].title.substr(6)).append(enemyStatData.skillpower[i]+' ');
     }
-	$('.carousel-indicators>li').first().attr("class", "active skill_active");
+	$('.carousel-indicators>li').first().attr("class", "active skill-active");
 	$('#inner0').attr("class", "item active");
 	
 	$("#myCarousel").carousel();
+	$("body").append(document.documentElement.clientWidth+" "+window.innerWidth);
 }
 
 function drawSkillArea(json, data)
